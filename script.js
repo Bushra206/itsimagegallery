c0cf1a02cfc6821695dc92560966f909128ea77a
Bushra206 <bushramaham2002@gmail.com>
2023-08-30 18:02:29 +0500

    itsimagegallery

---
+++ script.js
@@ -0,0 +1,12 @@
+
+               window.addEventListener("DOMContentLoaded",function(){
+               console.log("dom tree created");
+               });
+//  let sooper1=document.getElementById("sooper");
+//                sooper1.addEventListener("load",function(){
+//                 // alert("external item loaded");
+//                 alert("external item loaded");
+//                 });
+function load(){
+    alert("external item loaded");
+}
